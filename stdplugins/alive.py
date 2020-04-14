427ad55acdfc999f96ca396c67de39b336314eec
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-04-14 10:53:50 +0530

    Update alive.py

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -31,7 +31,7 @@ DEFAULTUSER = Config.ALIVE_NAME if Config.ALIVE_NAME else uname().node
 async def amireallyalive(on):
     """ For .on command, check if the bot is running.  """
     await on.edit(
-<<<<<<< HEAD
+
                      " 🤖Hey `I am ON Ashwin.`🤖\n"
                      " \n"
                      f"📱Telethon version: {version.__version__} \n"
@@ -40,16 +40,16 @@ async def amireallyalive(on):
                      f"👦🏻User: {DEFAULTUSER} \n"
                      " \n"
                      "🤖`I will never die.`🤖")    
-=======
-                     " Hey `i am 𝐎𝓷 My 𝕄𝕒𝕤𝕥𝕖𝕣`\n"
-                     " \n"
-                     f"тєℓєтнση νєяѕιση: {version.__version__} \n"
-                     f"P̳y̳t̳h̳o̳n̳ ̳v̳e̳r̳s̳i̳o̳n̳: {python_version()} \n"
-                     f"------------------------------------ \n"
-                     f"U̴̧̡̫̤̦̇͆͛̿͑̈́̂̊̚͝s̷̡͓͎͘e̷̹̙̝̽̾͂ŕ̴̡̛̺̖̝̬̣͖͕̐̅͌͂͌̕: {DEFAULTUSER} \n"
-                     f"🅲🆁🅴🅰🆃🅾🆁: @🄼🄰🅈🅄🅁_🄺🄰🅁🄰🄽🄸🅈🄰 \n"
-                     f"𝓞𝔀𝓷𝓮𝓻: `@𝓣𝓱𝓻𝓮𝓮_𝓒𝓾𝓫𝓮_𝓣𝓮𝓚𝓷𝓸𝔀𝓪𝔂𝓼` \n"
-                     f"ᗯEᗷᔕITE: 𝖍𝖙𝖙𝖕𝖘://𝖜𝖜𝖜.𝖋𝖆𝖈𝖊𝖇𝖔𝖔𝖐.𝖈𝖔𝖒/𝕿𝖊𝖐𝖓𝖔𝖜𝖆𝖞𝖘 \n"
-                     f"U҉s҉e҉r҉b҉o҉t҉: @ₜₑₛₜing_bₒₜ \n"
-                     "`𝘪 𝙘𝙖𝙣'𝙩 Ðïê`")    
->>>>>>> 6dbcf1165458cda768187ead535423b5791a29b1
+
+
+
+
+
+
+
+
+
+
+
+ 
+
